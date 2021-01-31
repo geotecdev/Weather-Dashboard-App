9e95446355ccce561c3f1e9a5403a194a0569714
Mike Ruane <mikeruanedev@gmail.com>
2021-01-31 15:46:26 -0500

    script.js get doc elements with id, add ui event listeners

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,39 @@
+let historyBtn;
+let searchInputEl;
+let searchBtn;
+let searchedCitiesModalEl;
+let modalBodyEl;
+let todaySectionTitleEl;
+let tempCaptionEl;
+let humidityCaptionEl;
+let windSpeedCaptionEl;
+let uvIndexCaptionEl;
+
+window.onload = function() {
+    //get document elements
+    historyBtn = document.querySelector("#historyBtn");
+    searchInputEl = document.querySelector("#searchInput");
+    searchBtn = document.querySelector("#searchBtn");
+    searchedCitiesModalEl = document.querySelector("#searchedCitiesModal");
+    modalBodyEl = document.querySelector("#modalBody");
+    todaySectionTitleEl = document.querySelector("#todaySectionTitle");
+    tempCaptionEl = document.querySelector("#tempCaption");
+    humidityCaptionEl = document.querySelector("#humidityCaption");
+    windSpeedCaptionEl = document.querySelector("#windSpeedCaption");
+    uvIndexCaptionEl = document.querySelector("#uvIndexCaption");
+
+    //attach event listeners
+
+    //ui events
+    searchInputEl.addEventListener("focus", function() {
+        searchInputEl.setAttribute("placeholder", "");      
+    });
+
+    searchInputEl.addEventListener("blur", function() {
+        searchInputEl.setAttribute("placeholder", "Search city");
+    })
+
+}
 
 
+// ## 
